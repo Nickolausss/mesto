6e69947635486df00f7b82f866d2b657da5d3cd2
Korotkov Nikolay <Korotkov-Nikolay.I@yandex.ru>
2022-12-12 15:23:41 +0300

    refactor: рефактор перед деплой

--- src/scripts/utils/arrays.js
+++ src/scripts/utils/arrays.js
@@ -1,19 +1,3 @@
-const greeceImage = new URL('../../images/element/element-greece.jpg', import.meta.url);
-const hongkongImage = new URL('../../images/element/element-hongkong.jpg', import.meta.url);
-const indonesiaImage = new URL('../../images/element/element-indonesia.jpg', import.meta.url);
-const koreaImage = new URL('../../images/element/element-korea.jpg', import.meta.url);
-const usaImage = new URL('../../images/element/element-usa.jpg', import.meta.url);
-const uaeImage = new URL('../../images/element/element-uae.jpg', import.meta.url);
-
-export const arrayCard = [
-	{ name: 'Греция', link: greeceImage },
-	{ name: 'Гонконг', link: hongkongImage },
-	{ name: 'Индонезия', link: indonesiaImage },
-	{ name: 'Южная Корея', link: koreaImage },
-	{ name: 'Соединенные Штаты Америки', link: usaImage },
-	{ name: 'Объединённые Арабские Эмираты', link: uaeImage }
-];
-
 export const listValidation = {
 	input: '.popup__item',
 	inputError: 'popup__item_type_error',
